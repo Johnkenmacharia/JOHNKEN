72b87b740c5543641c479eac36577d056b0fdcef
Johnkenmacharia <johnkenmacharia@gmail.com>
2023-02-23 10:22:41 +0300

    added changes

--- emails.py
+++ emails.py
@@ -1,5 +1,14 @@
-e1="johnkenmacharia@gmail.com"
-e2="stevenstanshylor@yahoo.com"
-e3="grace254@yahoo.com"
-e4="lewinski@gmail.com"
-e5="3122aandrew@yahoo.com"
+# name1 ="johnkenmacharia@gmail.com"
+# name2 ="stevenstanshylor@yahoo.com"
+# name3 ="grace254@yahoo.com"
+# name4 ="lewinski@gmail.com"
+# name5 ="3122aandrew@yahoo.com"
+
+
+user_email =input("enter email address")
+print (user_email[-10:])
+if user_email [-10:] =="@gmail.com":
+    print("correct email")
+
+else:
+    print("incorrect email")
